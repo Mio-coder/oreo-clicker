d9790157c6de64ceec482966dfb8a619fd3c3073
Mio-coder <78543744+Mio-coder@users.noreply.github.com>
2021-10-30 17:15:24 +0200

    update scenes

--- source/scripts/manager.py
+++ source/scripts/manager.py
@@ -4,9 +4,12 @@ from typing import Union
 import pygame as pg
 from pygame import Surface, event
 from pygame.display import set_mode, set_caption, set_icon, get_surface, update
-from pygame.time import Clock
+from pygame.key import get_pressed as get_key_pressed
+from pygame.mouse import get_pressed as get_mouse_pressed
+from pygame.time import Clock, get_ticks
 from pygame.transform import scale
 
+from source.imgs.sprite_sheet import load_sprite_sheet
 from source.scripts.scenes import Game, Menu, Shop, Scene
 from source.sounds.manager import get_song
 
@@ -16,31 +19,51 @@ class App:
                          filter(lambda k: k.startswith("K_"), dir(pg.constants)))
 
     def __init__(self, config: dict[str, Union[int, str]] = None):
+        # ## load
+        # # load config
         if config is None:
             with open("source/config.json") as file:
                 config = load(file)
 
+        # get config
         self.width: int = ...
-        self.high: int = ...
+        self.height: int = ...
         self.fps: int = ...
         self.start_scene: str = ...
+        self.volume: int = ...
 
-        for name in ["width", "high", "fps", "start_scene"]:
+        for name in ["width", "height", "fps", "start_scene", "volume"]:
             setattr(self, name, config[name])
+
+        self.done = True
+        self.clock = Clock()
+
+        # # load images
+        self.bg_sprite_sheet = load_sprite_sheet("bg_imgs")
+        # ## create and initialize
+        # # create music
+
+        self.bgm_intro = get_song("intro.wav")
+        self.bgm = get_song("bgm.wav")
+        self.bgm_is_running = False
+
+        self.bgm_intro.set_volume(self.volume)
+        self.bgm.set_volume(self.volume)
+
+        # # create scenes
+        self.name = "OREO Clicker"
+        self.screen: Surface
 
         self.game = Game(self)
         self.menu = Menu(self)
         self.shop = Shop(self)
-        self._scene: Scene = ...
 
-        self.done = True
-        self.clock = Clock()
-        self.screen: Surface
+        self._scene: Scene = getattr(self, self.start_scene, "menu")
 
-        self._scene = getattr(self, self.start_scene, "menu")
-        self.bgm_intro = get_song("source/songs/intro.wav")
-        self.bgm = get_song("source/sounds/bgm.wav")
-        self.bgm.is_running = False
+        # initialize scenes
+        self.game.initialize()
+        self.menu.initialize()
+        self.shop.initialize()
 
     @property
     def scene(self) -> Scene:
@@ -49,8 +72,6 @@ class App:
     @scene.setter
     def scene(self, value: Scene):
         self._scene = value
-        if not self.done:
-            self._scene.initialize()
         self.update_screen()
 
     @property
@@ -67,8 +88,9 @@ class App:
         # noinspection PyAttributeOutsideInit
         self.screen = get_surface()
 
-    def get_scene_screen(self):
-        return Surface(self.scene.settings["size"])
+    @property
+    def scene_screen(self):
+        return Surface(self.game.settings["size"])
 
     def draw(self):
         self.screen.blit(scale(self.scene.draw(), self.scene.settings["scale"]), (0, 0))
@@ -78,18 +100,19 @@ class App:
         self.scene.update()
 
     def run(self):
-        self.bgm_intro.play(1)
+        self.bgm_intro.play()
         self.done = False
-        self._scene.initialize()
+        self.update_screen()
         while not self.done:
-            if self.clock.get_time() <= self.bgm_intro.get_length() and not self.bgm.is_running:
+            if get_ticks() // (self.fps * 20) >= int(self.bgm_intro.get_length()) and not self.bgm_is_running:
+                self.bgm_intro.stop()
                 self.bgm.play(-1)
-                self.bgm.is_running = True
-            self.update()
+                self.bgm_is_running = True
             self.draw()
             self.handle_events()
             self.handle_input()
             self.clock.tick(self.fps)
+            self.update()
 
     def handle_events(self):
         for event_ in event.get():
@@ -101,18 +124,18 @@ class App:
 
     def handle_input(self):
         self.handle_mouse_press()
-        keys_pressed = pg.key.get_pressed()
+        keys_pressed = get_key_pressed()
         for keycode in self.all_keycodes:
             if keys_pressed[keycode]:
                 self.scene.handle_input(keycode)
 
     def handle_mouse_press(self):
-        pressed = pg.mouse.get_pressed(3)
+        pressed = get_mouse_pressed(3)
         mouse_pos = self.get_mouse_pos()
         for key in range(3):
             if pressed[key]:
                 self.scene.handle_mouse_press(key, mouse_pos)
 
     def get_mouse_pos(self):
-        return pg.mouse.get_pos()[0] // self.scene_scale[0],\
+        return pg.mouse.get_pos()[0] // self.scene_scale[0], \
                pg.mouse.get_pos()[1] // self.scene_scale[1]
--- source/scripts/scenes.py
+++ source/scripts/scenes.py
@@ -1,20 +1,21 @@
+from pygame import Surface
+
 from source.scripts.button import Button
-from source.scripts.main import App
 from source.sounds.manager import get_song
 
-click_sound = get_song("source/sounds/click.wav")
+click_sound = get_song("click.wav")
 
 
 class Scene:
-    settings = {"scale": ..., "size": ...}
+    settings = {"scale": ..., "size": ..., "icon": None}
 
     def __init__(self, app):
         self.app = app
 
-    def render(self):
+    def initialize(self):
         pass
 
-    def initialize(self):
+    def render(self):
         pass
 
     def update(self):
@@ -34,20 +35,27 @@ class Scene:
 
 
 class Menu(Scene):
+    settings = {"scale": (500, 900), "size": (500, 900), "title": "{app.name} - Menu", "icon": None}
 
-    def __init__(self, app: App):
+    def __init__(self, app):
         Scene.__init__(self, app)
-        self.settings = {"scale": ..., "size": ..., "title": ...}
+        self.settings["title"].format(app=app)
+        self.screen: Surface = ...
+        self.button_game: Button = ...
+        # self.button_settings: Button = ...
+
+    def initialize(self):
+        self.screen = self.app.scene_screen
 
         def game(_):
             self.app.scene = self.app.menu
 
-        def settings(_):
-            # self.app.scene = self.app.settings
-            pass
+        # def settings(_):
+        #     # self.app.scene = self.app.settings
+        #     pass
 
         self.button_game = Button(
-            self.app.screen,
+            self.screen,
             (300, 200),
             ["menu"],
             action=game
@@ -60,28 +68,117 @@ class Menu(Scene):
         #     action=settings
         # )
 
-    def draw(self):
+    def render(self):
         self.button_game.render()
+
+    def draw(self):
+        return self.screen
+
+
+class Clicker:
+
+    def __init__(self, game, num):
+        self.game = game
+        self.num = num
+        self.screen: Surface = ...
+
+    def initialize(self):
+        self.screen = Surface((
+            self.game.settings["size"][0] // 3 * self.num,
+            self.game.settings["size"][1]
+        ))
+
+    def draw(self):
+        return self.screen
+
+    def handle_mouse_press(self, key, mouse_pos):
+        print(f"click in {self.__class__.__name__}: key {key} at {mouse_pos}")
+
+
+class MilkClicker(Clicker):
+
+    def __init__(self, game):
+        Clicker.__init__(self, game, 1)
+
+    def draw(self):
+        self.screen.fill((40, 40, 40))
+        return self.screen
+
+
+class CookieClicker(Clicker):
+
+    def __init__(self, game):
+        Clicker.__init__(self, game, 3)
+
+    def draw(self):
+        self.screen.fill((200, 40, 40))
+        return self.screen
+
+
+class OREOClicker(Clicker):
+
+    def __init__(self, game):
+        Clicker.__init__(self, game, 2)
+
+    def draw(self):
+        return self.screen
 
 
 class Game(Scene):
+    settings = {"scale": (1500, 900), "size": (1500, 900), "title": "{app.name} - Game", "icon": None}
 
-    def __init__(self, app: App):
+    def __init__(self, app):
         Scene.__init__(self, app)
-        self.settings = {"scale": ..., "size": ..., "title": ...}
+        self.settings["title"].format_map({"app": app})
+        self.screen: Surface = ...
+
+        self.milkClicker: MilkClicker = ...
+        self.cookieClicker: CookieClicker = ...
+        self.oreoClicker: OREOClicker = ...
+
+    def initialize(self):
+        self.screen = self.app.get_scene_screen()
+
+        self.milkClicker = MilkClicker(self)
+        self.cookieClicker = CookieClicker(self)
+        self.oreoClicker = OREOClicker(self)
+
+        self.milkClicker.initialize()
+        self.cookieClicker.initialize()
+        self.oreoClicker.initialize()
+
+    def draw(self):
+        milk_screen = self.milkClicker.draw()
+        self.screen.blit(milk_screen, (0, 0), milk_screen.get_rect())
+        return self.screen
 
     def handle_mouse_press(self, key, mouse_pos):
         click_sound.play()
+        print(self.cookieClicker.screen.get_rect())
+        breakpoint()
+        if self.milkClicker.screen.get_rect().collidepoint(mouse_pos):
+            self.milkClicker.handle_mouse_press(key, [mouse_pos[0] - self.settings["size"][0] * 0, mouse_pos[1]])
+        if self.cookieClicker.screen.get_rect().collidepoint(mouse_pos):
+            self.milkClicker.handle_mouse_press(key, [mouse_pos[0] - self.settings["size"][0] * 1, mouse_pos[1]])
+        if self.oreoClicker.screen.get_rect().collidepoint(mouse_pos):
+            self.milkClicker.handle_mouse_press(key, [mouse_pos[0] - self.settings["size"][0] * 2, mouse_pos[1]])
+
+    def update(self):
+        pass
 
 
 class Shop(Scene):
+    settings = {"scale": (400, 500), "size": (400, 500), "title": "{app.name} - Shop", "icon": None}
 
-    def __init__(self, app: App):
+    def __init__(self, app):
         Scene.__init__(self, app)
-        self.settings = {"scale": ..., "size": ..., "title": "Shop"}
+        self.settings["title"].format(app=app)
+
+    def initialize(self):
+        pass
 
 
 class Settings(Scene):
 
-    def __init__(self, app: App):
+    def __init__(self, app):
         Scene.__init__(self, app)
