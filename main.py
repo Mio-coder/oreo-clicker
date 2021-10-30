d4df29730d17f6380e868b86fa31438d27a6e030
Mio-coder <78543744+Mio-coder@users.noreply.github.com>
2021-10-30 17:16:07 +0200

    update main

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-from source.scripts.main import App
 from json import load
 
+import pygame as pg
+
 if __name__ == '__main__':
+    pg.init()
+    from source.scripts.manager import App
+
     with open("source/config.json") as file:
         data: dict = load(file)
     app = App(data)
+    app.run()
